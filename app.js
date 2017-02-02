a890a77fb1cc3579407bbcc4226eeb550395078b
san <metternichch@gmail.com>
2017-02-02 20:39:06 +0900

    rush!

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var express    = require('express');
+var app        = express();
+var path       = require('path');
+var fs         = require('fs');
 var bodyParser = require('body-parser');
+var favicon    = require('serve-favicon');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// quickly route address
+var index     = require('./routes/index');
+var users     = require('./routes/users');
 var templates = require('./routes/templates');
-
-var app = express();
+var favicon   = require('serve-favicon');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(express.static(path.join(__dirname + "/")));
+app.use(favicon(path.join(__dirname, 'images/favicon', 'goodstudio.ico')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', index);
 app.use('/users', users);
 app.use('/templates', templates);
+
+// templete loading
+app.post('/templete', function (req, res) {
+  console.log("success");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
+});
+
+app.post('/template', function (req, res) {
+
+  var subject = req.body.subject;
+  var url     = req.body.url;
+  var enexUrl = req.body.enexUrl;
+
+  console.log("subject : " + subject);
+  console.log("url     : " + url);
+  console.log("enexUrl : " + enexUrl);
+
 });
 
 // error handler
@@ -45,7 +58,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// port 8082 setting. if you change port, change port num.
+app.listen(8082, function () {
+    console.log('NodeJs Server START on port 8082!');
+});
 
-
-
+//module.exports = app;
